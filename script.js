0e991293c4d8f391be741697a87bee32fbe7cb4a
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-01-16 22:21:08 +0000

    Add getComputerChoice()
    Returns "rock", "paper" or "scissors" based on getRandomInt() integer return.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
+function getComputerChoice() {
+  const numberRoll = getRandomInt(3);
+  if (numberRoll === 0) return "rock";
+  if (numberRoll === 1) return "paper";
+  if (numberRoll === 2) return "scissors";
+}
