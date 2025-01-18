e6363cb4a80c110d58a9f9c7b5f5a094f0fb6c11
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-01-18 20:27:05 +0000

    Create playRound function for single round game
    Function will play single round of game and return winner.

--- script.js
+++ script.js
@@ -16,3 +16,37 @@ function getHumanChoice() {
   const choice = prompt("Rock, Paper or Scissors?");
   return choice;
 }
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) console.log("Draw!");
+
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win!, Rock beats Scissors");
+    humanScore++;
+  }
+
+  if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win!, Paper beats Rock");
+    humanScore++;
+  }
+
+  if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win!, Scissors beats Paper");
+    humanScore++;
+  }
+
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose!, Paper beats Rock");
+    computerScore++;
+  }
+
+  if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You lose!, Scissors beats Paper");
+    computerScore++;
+  }
+
+  if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose!, Rock beats Scissors");
+    computerScore++;
+  }
+}
